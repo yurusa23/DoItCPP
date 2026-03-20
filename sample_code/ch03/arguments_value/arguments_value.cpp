b3ddeaff23c7f26d4ed64efc08020bceef360ec6
KalSIT811 <dieson@naver.com>
2026-03-21 00:38:52 +0900

    C++ 메모리/포인터 학습 예제 및 주석, 메모 추가
    여러 예제 파일에 한글 주석을 추가하여 포인터, 배열, 동적 메모리 등 핵심 개념의 이해를 돕고, Visual Studio 솔루션에 practice 그룹과 C++practice 프로젝트를 신규 추가했습니다.
    동적 배열 예제(3-1.cpp)와 메모리 구조 설명용 Memo.txt 파일도 포함되어 초보자 학습에 최적화된 실습 환경을 제공합니다.

--- sample_code/ch03/arguments_value/arguments_value.cpp
+++ sample_code/ch03/arguments_value/arguments_value.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-void change_negative(int _val) {
-  if (_val > 0) {
-    _val = -_val;
-  }
+void change_negative(int _val) { // 매개변수로 값을 전달
+	if (_val > 0) { // 매개변수로 전달된 값이 양수인 경우
+		_val = -_val; // 매개변수로 전달된 값을 음수로 변경
+  } 
 }
 
-int main() {
-  int a = 3, b = -3;
+int main() { // 매개변수로 값을 전달하는 경우
+	int a = 3, b = -3; // a와 b 변수에 각각 3과 -3을 저장
 
-  cout << "a : " << a << endl;
-  cout << "b : " << b << endl;
+	cout << "a : " << a << endl; // a 변수의 값을 출력
+	cout << "b : " << b << endl;// b 변수의 값을 출력
 
-  change_negative(a);
-  change_negative(b);
+	change_negative(a); // a 변수의 값을 매개변수로 전달
+	change_negative(b); // b 변수의 값을 매개변수로 전달
 
-  cout << "change_negative(a) : " << a << endl;
-  cout << "change_negative(b) : " << b << endl;
-
+	cout << "change_negative(a) : " << a << endl; // change_negative 함수가 a 변수의 값을 매개변수로 전달했지만, a 변수의 값은 변경되지 않음
+	cout << "change_negative(b) : " << b << endl; // change_negative 함수가 b 변수의 값을 매개변수로 전달했지만, b 변수의 값은 변경되지 않음
+	// change_negative 함수가 매개변수로 전달된 값을 변경하려고 했지만, 매개변수로 전달된 값은 복사되어 전달되기 때문에, a와 b 변수의 값은 변경되지 않음
   return 0;
 }

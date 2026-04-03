497e67e97f44c89e9d16574dea2eb3b482d23976
선호 박 <grayzero03@gmail.com>
2026-04-03 14:52:43 +0900

    ch05 연습문제 1,2

---
+++ sample_code/ch05problems/problem01.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	try {
+		int divisor = 0;
+
+		if (divisor == 0) {
+			throw std::runtime_error("0으로 나눌 수 없습니다."); // 예외 발생
+		}
+		int result = 10 / divisor;
+		std::cout << "결과: " << result << std::endl;
+	}
+	catch (const std::runtime_error& e) {
+		// 예외 발생 시 처리
+		std::cout << "예외 발생: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
---
+++ sample_code/ch05problems/problem02.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void process(int value) {
+	try {
+		if (value < 0) { // value가 음수인 경우 예외 발생
+			throw std::runtime_error("음수는 처리할 수 없습니다."); // 예외 객체 생성하여 예외 발생
+		}
+		std::cout << "value : " << value << std::endl;
+	}
+	catch (const std::runtime_error& e) { 
+		std::cout << "예외 발생: " << e.what() << std::endl; // 예외 메시지 출력
+	}
+
+	// value가 음수인 경우 예외를 던지도록 처리
+	// 예외가 발생하면 "예외 발생!" 출력, 발생하지 않으면 value 출력
+}
+
+int main() {
+	process(10);
+	process(-5);
+	return 0;
+}
